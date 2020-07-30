9c96cd7aab0a01b1b30d39a14f82218e84f1acd0
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-07-30 19:27:48 +0800

    complete concolic exploration

--- main/concolic.py
+++ main/concolic.py
@@ -189,11 +189,26 @@ def run_concolic_exploration(program, argument_list, second_var_list, root_direc
     run_concolic_execution(program, argument_list, second_var_list, print_output=False)
     ppc_log_path = root_directory + "/klee-last/ppc.log"
     is_initial = True
+    path_count = 1
     while list_path_detected or is_initial:
         is_initial = False
-        new_arg_list, new_var_list = generate_new_input(ppc_log_path, root_directory)
+        path_count = path_count + 1
+        gen_arg_list, gen_var_list = generate_new_input(ppc_log_path, root_directory)
+        input_var_list = list()
+        input_arg_list = list()
+        for i in range(0, len(gen_arg_list)):
+            arg_name = "arg0" + str(i)
+            arg_value = str(gen_arg_list[arg_name]).split("(")[1].split("_")[0]
+            print(arg_name, arg_value)
+            input_arg_list.append(arg_value)
 
-        print(new_arg_list)
-        print(new_var_list)
+        for var_name in gen_var_list:
+            var_str = str(gen_var_list[var_name])
+            var_size = str(int(var_str.split("BV{")[1].split("}")[0])/8)
+            var_value = var_str.split("(")[1].split("_")[0]
+            print(var_name, var_size, var_value)
+            input_var_list.append({"identifier": var_name, "value": var_value, "size": var_size})
 
+        run_concolic_execution(program, input_arg_list, input_var_list)
 
+    print("Explored {0} number of paths".format(path_count))
