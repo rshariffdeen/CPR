8d82f1eba5f19551889ec7a1b229899ead4dc487
Yannic Noller <nolleryc@gmail.com>
2020-08-03 18:13:25 +0200

    added example

---
+++ tests/example/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int main(int argc, char *argv[]) {
+  int x = atoi(argv[1]);
+  int y = __trident_choice("L9", "bool", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0);
+  int res;
+  if (y)  {
+      res = 1;
+  } else {
+      res = 0;
+  }
+  return TRIDENT_OUTPUT("res", "i32", res);
+}
