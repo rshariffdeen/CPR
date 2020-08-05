ff21d7619a3cfced6a8f1046798776de70cbf51a
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-08-05 15:30:43 +0800

    change concolic to concrete

--- tests/example/run.py
+++ tests/example/run.py
@@ -2,7 +2,7 @@ import subprocess
 import os
 import sys
 sys.path.append('/concolic-repair/main')
-from concolic import generate_ktest, run_concolic_execution
+from concolic import generate_ktest, run_concolic_execution, run_concrete_execution
 from utilities import build_program
 
 program_name = "test"
@@ -14,29 +14,29 @@ assert int(build_status) == 0
 assert os.path.isfile(program_path)
 assert os.path.getsize(program_path) > 0
 
-second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 16843009, "size": 4}]
+# second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 16843009, "size": 4}]
 argument_list = [1]
-ktest_path, exit_code = generate_ktest(argument_list, second_var_list)
+ktest_path, exit_code = generate_ktest(argument_list, {})
 assert int(exit_code) == 0
 assert os.path.isfile(ktest_path)
 assert os.path.getsize(ktest_path) > 0
-exit_code = run_concolic_execution(program_name + ".bc", argument_list, second_var_list, True)
+exit_code = run_concrete_execution(program_name + ".bc", argument_list, {}, True)
 assert exit_code == 0
 klee_file_1 = cwd + "/klee-out-0/test000001.smt2"
 assert os.path.isfile(klee_file_1)
 assert os.path.getsize(klee_file_1) > 0
 
-second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 0, "size": 4}]
-argument_list = [1]
-ktest_path, exit_code = generate_ktest(argument_list, second_var_list)
-assert int(exit_code) == 0
-assert os.path.isfile(ktest_path)
-assert os.path.getsize(ktest_path) > 0
-exit_code = run_concolic_execution(program_name + ".bc", argument_list, second_var_list, True)
-assert exit_code == 0
-klee_file_2 = cwd + "/klee-out-1/test000001.smt2"
-assert os.path.isfile(klee_file_2)
-assert os.path.getsize(klee_file_2) > 0
+# second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 0, "size": 4}]
+# argument_list = [1]
+# ktest_path, exit_code = generate_ktest(argument_list, second_var_list)
+# assert int(exit_code) == 0
+# assert os.path.isfile(ktest_path)
+# assert os.path.getsize(ktest_path) > 0
+# exit_code = run_concolic_execution(program_name + ".bc", argument_list, second_var_list, True)
+# assert exit_code == 0
+# klee_file_2 = cwd + "/klee-out-1/test000001.smt2"
+# assert os.path.isfile(klee_file_2)
+# assert os.path.getsize(klee_file_2) > 0
 
 synthesis_command = "python3.6 ../../main/synthesis.py \
           --tests t1.smt2:klee-out-0 t2.smt2:klee-out-1 \
