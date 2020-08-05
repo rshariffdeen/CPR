0cdb282753c802bb30c8f2e9ca9bc96dbd60e326
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-08-05 13:30:32 +0800

    update test case

--- tests/example/run.py
+++ tests/example/run.py
@@ -14,7 +14,7 @@ assert int(build_status) == 0
 assert os.path.isfile(program_path)
 assert os.path.getsize(program_path) > 0
 
-second_var_list = []
+second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 16843009, "size": 4}]
 argument_list = [1]
 ktest_path, exit_code = generate_ktest(argument_list, second_var_list)
 assert int(exit_code) == 0
@@ -26,8 +26,20 @@ klee_file_1 = cwd + "/klee-out-0/test000001.smt2"
 assert os.path.isfile(klee_file_1)
 assert os.path.getsize(klee_file_1) > 0
 
+second_var_list = [{"identifier": "choice!angelic!bool!L9!0", "value": 0, "size": 4}]
+argument_list = [1]
+ktest_path, exit_code = generate_ktest(argument_list, second_var_list)
+assert int(exit_code) == 0
+assert os.path.isfile(ktest_path)
+assert os.path.getsize(ktest_path) > 0
+exit_code = run_concolic_execution(program_name + ".bc", argument_list, second_var_list, True)
+assert exit_code == 0
+klee_file_2 = cwd + "/klee-out-1/test000001.smt2"
+assert os.path.isfile(klee_file_2)
+assert os.path.getsize(klee_file_2) > 0
+
 synthesis_command = "python3.6 ../../main/synthesis.py \
-          --tests t1.smt2:klee-out-0 \
+          --tests t1.smt2:klee-out-0 t2.smt2:klee-out-1 \
           --components components/*.smt2 ../../components/less-than.smt2 ../../components/constant_a.smt2  \
           --all"
         #   --all-values ' -10:11' --output patches"
