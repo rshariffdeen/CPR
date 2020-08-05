98c7d7abd96b2ac93b24dc675a2e2a7e50e9c377
Yannic Noller <nolleryc@gmail.com>
2020-08-05 12:20:43 +0200

    updated repair skeleton

--- main/repair.py
+++ main/repair.py
@@ -1,28 +1,35 @@
-import os
-import synthesis as trident
-from concolic import run_concolic_exploration, run_concolic_execution, generate_new_input
 import subprocess
+import os
 import sys
+sys.path.append('/concolic-repair/main')
+from concolic import generate_ktest, run_concolic_execution, run_concrete_execution, generate_new_input
+from utilities import build_program
+import synthesis as trident
 
-
-def init (working_path):
-   os.chdir(working_path)
+def init (cwd, program_name):
+   os.chdir(cwd)
    os.system("rm -rf ./patches/*")
+   os.system("rm -rf ./klee*")
 
-   #sys.path.append('/concolic-repair/main')
+   program_path = cwd + "/" + program_name
 
    ## Build the program under test.
-   os.system("./build")
-   # compile_command = "export LLVM_COMPILER=clang;" \
-   #                   "wllvm -l kleeRuntest -o -g -O0 test test.c;" \
-   #                   "extract-bc test"
-   # process = subprocess.Popen([compile_command], stderr=subprocess.PIPE, shell=True)
-   # (output, error) = process.communicate()
-   # assert int(process.returncode) == 0
+   build_status = build_program(program_path)
+   assert int(build_status) == 0
+   assert os.path.isfile(program_path)
+   assert os.path.getsize(program_path) > 0
 
    ## Execute klee with this program so to generate the expected results for test cases.
-   # klee --posix-runtime --libc=uclibc --write-smt2s --output-dir=klee-t1 "${PROGRAM}.bc" 1
-   os.system("klee --posix-runtime --libc=uclibc --write-smt2s --output-dir=klee-t1 'test.bc' 1")
+   argument_list = [1]
+   ktest_path, exit_code = generate_ktest(argument_list, {})
+   assert int(exit_code) == 0
+   assert os.path.isfile(ktest_path)
+   assert os.path.getsize(ktest_path) > 0
+   exit_code = run_concrete_execution(program_name + ".bc", argument_list, {}, True)
+   assert exit_code == 0
+   klee_file_1 = cwd + "/klee-out-0/test000001.smt2"
+   assert os.path.isfile(klee_file_1)
+   assert os.path.getsize(klee_file_1) > 0
 
 
 def reduce(): # TODO
@@ -43,38 +50,49 @@ def checkCoverage(): # TODO
 
 def main():
 
-   project_path = "/concolic-repair/tests/example/"
-   
+   project_path = "/concolic-repair/tests/example"
+   program = "test"
+
    # Prepare the program under test.
-   init(project_path)
+   init(project_path, program)
 
    ## Generate all possible solutions by running the synthesizer.
-   # trident.main(["--tests", "t1.smt2:klee-t1", "--components", "components/*.smt2", "../../components/less-than.smt2", "../../components/constant_a.smt2", "--all", "--output", "patches"])
-   trident.main(["--tests", "t1.smt2:klee-t1", "--components", "components/x.smt2", "../../components/less-than.smt2", "../../components/constant_a.smt2", "--all", "--output", "patches"])
+   synthesis_arguments = [
+      "--tests", "t1.smt2:klee-out-0",
+      "--components", "components/x.smt2", "../../components/less-than.smt2", "../../components/constant_a.smt2",
+      "--all-values", " -10:11",
+      "--output", "patches"
+      ]
+   trident.main(synthesis_arguments)
 
    numberOfPatches = len(os.listdir("./patches"))
    print("|P|=" + str(numberOfPatches))
 
-   # Log file for the previous concolic execution that captures PPC.
-   log_path = "" # TODO how do we init the log_path?
+   # TODO this is a dummy call to produce the first results
+   global list_path_explored, list_path_detected
+   second_var_list = [{"identifier": "x", "value": 1, "size": 4}]
+   argument_list = [1]
+   run_concolic_execution(program + ".bc", argument_list, second_var_list, True)
+   ppc_log_path = project_path + "/klee-last/ppc.log"
+   print(">> " + str(ppc_log_path))
+
+   print(">> start repair loop")
 
    satisfied = numberOfPatches <= 1
    while not satisfied:
 
       # Pick new input and patch candidate for next concolic execution step.
-      # input_arg_list, input_var_list = generate_new_input(log_path, project_path) #TODO patch candidate missing
+      gen_arg_list, gen_var_list = generate_new_input(ppc_log_path, project_path) #TODO (later) patch candidate missing
 
       # Concolic execution of concrete input and patch candidate to retrieve path constraint.
-      second_var_list = [{"identifier": "x", "value": 1, "size": 4}]
-      argument_list = [1]
-      status = run_concolic_execution("test.bc", argument_list, second_var_list, print_output=True)
+      status = run_concolic_execution(program + ".bc", gen_arg_list, gen_var_list)
       print(status)
 
-      # 
+      # Reduces the set of patch candidates based on the current path constraint
+      reduce()
 
-      
+      # Checks for the current coverage.
       satisfied = checkCoverage()
-
 
 
    print("Finished.")
@@ -82,3 +100,9 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
+
+
