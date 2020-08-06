a0d014b0ca7d57b1dc07b7af771e8a1ead74da8f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-08-06 23:31:12 +0800

    fix bugs

--- main/utilities.py
+++ main/utilities.py
@@ -42,59 +42,57 @@ def z3_get_model(formula):
     model_byte_list = parse_z3_output(z3_output)
     print(model_byte_list)
 
+
 def parse_z3_output(z3_output):
     model = dict()
     collect_lambda = False
     var_name = ""
+    old_var_name = ""
     byte_list = dict()
     str_lambda = ""
     for line in z3_output:
         if "define-fun " in line:
+            if str_lambda:
+                if "const" in str_lambda:
+                    str_value = str_lambda.split("#x")[-1].split(")")[0]
+                    byte_list = dict()
+                    byte_list[0] = int("0x" + str_value, 16)
+                elif "ite" in str_lambda:
+                    max_index = 0
+                    byte_list = dict()
+                    token_list = str_lambda.split("(ite (= x!1 ")
+                    for token in token_list[1:]:
+                        if token.count("#x") == 2:
+                            index, value = token.split(") ")
+                        elif token.count("#x") == 3:
+                            index, value, default = token.split(" ")
+                            index = index.replace(")", "")
+                            default = default.split(")")[0].replace("#", "0")
+                        index = index.replace("#", "0")
+                        value = value.replace("#", "0")
+                        index = int(index, 16)
+                        if index > max_index:
+                            max_index = index
+                        byte_list[index] = int(value, 16)
+                    for i in range(0, max_index):
+                        if i not in byte_list:
+                            byte_list[i] = int(default, 16)
+
+                else:
+                    print("Unhandled output")
+                    print(str_lambda)
+                    print(z3_output)
+                    exit(1)
+
+            if var_name and byte_list:
+                model[var_name] = byte_list
+                var_name = ""
+                byte_list = dict()
             var_name = line.split("define-fun ")[1].split(" ()")[0]
-            collect_lambda = False
-        if collect_lambda:
+            str_lambda = ""
+
+        else:
             str_lambda += line
-        if "lambda " in line or "as const " in line:
-            collect_lambda = True
-            str_lambda = line
-
-        if (not collect_lambda and str_lambda) or (line == z3_output[-1]):
-            if "const" in str_lambda:
-                str_value = str_lambda.split("#x")[-1].split(")")[0]
-                byte_list = dict()
-                byte_list[0] = int("0x" + str_value, 16)
-            elif "ite" in str_lambda:
-                max_index = 0
-                byte_list = dict()
-                token_list = str_lambda.split("(ite (= x!1 ")
-                for token in token_list[1:]:
-                    if token.count("#x") == 2:
-                        index, value = token.split(") ")
-                    elif token.count("#x") == 3:
-                        index, value, default = token.split(" ")
-                        index = index.replace(")", "")
-                        default = default.split(")")[0].replace("#", "0")
-                    index = index.replace("#", "0")
-                    value = value.replace("#", "0")
-                    index = int(index, 16)
-                    if index > max_index:
-                        max_index = index
-                    byte_list[index] = int(value, 16)
-                for i in range(0, max_index):
-                    if i not in byte_list:
-                        byte_list[i] = int(default, 16)
-
-            else:
-                print("Unhandled output")
-                print(str_lambda)
-                print(z3_output)
-                exit(1)
-
-        if var_name and byte_list:
-            model[var_name] = byte_list
-            var_name = ""
-            byte_list = dict()
-
 
     return model
 
