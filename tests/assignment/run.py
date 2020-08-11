5b15473a082062f8d4f65db2d5bbd7af5a5352c1
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-08-11 14:06:35 +0800

    replace cli with api

--- tests/assignment/run.py
+++ tests/assignment/run.py
@@ -3,7 +3,10 @@ import os
 import sys
 sys.path.append('/concolic-repair/main')
 from concolic import generate_ktest, run_concolic_execution
+from synthesis import load_components, load_specification, synthesize, Program
 from utilities import build_program
+from pathlib import Path
+from typing import List, Dict, Tuple
 
 
 program_name = "test"
@@ -42,11 +45,36 @@ klee_file_2 = cwd + "/klee-out-1/test000001.smt2"
 assert os.path.isfile(klee_file_2)
 assert os.path.getsize(klee_file_2) > 0
 
-synthesis_command = "python3.6 ../../main/synthesis.py \
-          --tests t1.smt2:klee-out-0 t2.smt2:klee-out-1 \
-          --components components/*.smt2 ../../components/assignment.smt2 ../../components/addition.smt2 ../../components/constant_a.smt2 \
-          --all"
 
-process = subprocess.Popen([synthesis_command], stderr=subprocess.PIPE, shell=True)
-(output, error) = process.communicate()
-assert int(process.returncode) == 0
+# synthesis_command = "python3.6 ../../main/synthesis.py \
+#           --tests t1.smt2:klee-out-0 t2.smt2:klee-out-1 \
+#           --components components/*.smt2 ../../components/assignment.smt2 ../../components/addition.smt2 ../../components/constant_a.smt2 \
+#           --all"
+#
+# process = subprocess.Popen([synthesis_command], stderr=subprocess.PIPE, shell=True)
+# (output, error) = process.communicate()
+# assert int(process.returncode) == 0
+
+
+comp_files = []
+comp_files.append(Path("components/x.smt2"))
+comp_files.append(Path("../../components/assignment.smt2"))
+comp_files.append(Path("../../components/addition.smt2"))
+comp_files.append(Path("../../components/constant_a.smt2"))
+components = load_components(comp_files)
+
+depth = 3
+
+spec_files = []
+spec_files.append((Path("t1.smt2"), Path("klee-out-0")))
+spec_files.append((Path("t2.smt2"), Path("klee-out-1")))
+specification = load_specification(spec_files)
+
+concrete_enumeration = True
+lower_bound = -10
+upper_bound = 11
+
+result = synthesize(components, depth, specification, concrete_enumeration, lower_bound, upper_bound)
+list_of_patches = [_ for _ in result]
+print(list_of_patches)
+
