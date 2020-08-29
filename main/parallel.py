49f7bf44412305c3ce7cf8a604d63e716368d91a
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-08-29 10:55:50 +0800

    avoid negating  trident assumptions

--- main/parallel.py
+++ main/parallel.py
@@ -1,5 +1,5 @@
 import multiprocessing as mp
-from main import emitter, oracle
+from main import emitter, oracle, definitions
 
 pool = mp.Pool(mp.cpu_count())
 results = []
@@ -16,6 +16,8 @@ def generate_symbolic_paths_parallel(ppc_list):
     results = []
     pool = mp.Pool(mp.cpu_count())
     for control_loc in ppc_list:
+        if definitions.DIRECTORY_RUNTIME in control_loc:
+            continue
         ppc_list_at_loc = ppc_list[control_loc]
         for ppc in ppc_list_at_loc:
             pool.apply_async(oracle.check_path_feasibility, args=(control_loc, ppc), callback=collect_result)
