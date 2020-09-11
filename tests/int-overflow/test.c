eec58c596e1117773013b14e90e37332e2596aa5
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-09-11 13:19:30 +0800

    rename test

---
+++ tests/int-overflow/test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int main(int argc, char *argv[]) {
+  int x = atoi(argv[1]);
+  int res, y;
+  if (__trident_choice("L9", "bool", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0))  {
+      return -1;
+  }
+  y = x - 1;
+  TRIDENT_OUTPUT("y", "i32", y);
+  res = 1000 / y;
+  return 0;
+}
