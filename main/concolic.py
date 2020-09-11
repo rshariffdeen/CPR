aeb3ca95f6cc1559ae062bcf41d58adbe70cb3a5
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-09-11 10:48:05 +0800

    keep file extension for generated files

--- main/concolic.py
+++ main/concolic.py
@@ -303,7 +303,8 @@ def generate_binary_file(byte_array):
     byte_list = []
     for i in range(0, len(byte_array)):
         byte_list.append(byte_array[i])
-    values.FILE_POC_GEN = definitions.DIRECTORY_OUTPUT + "/input-" + str(values.ITERATION_NO)
+    file_extension = str(values.CONF_PATH_POC).split(".")[-1]
+    values.FILE_POC_GEN = definitions.DIRECTORY_OUTPUT + "/input-" + str(values.ITERATION_NO) + "." + file_extension
     with open(values.FILE_POC_GEN, "wb") as new_input_file:
         new_input_file.write(bytearray(byte_list))
 
