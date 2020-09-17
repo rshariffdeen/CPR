320f37122f9679ab2f996ca0ba79d709ec0221a7
rshariffdeen <rshariffdeen@gmail.com>
2020-09-17 14:35:12 +0800

    update script

--- experiments/extractfix/coreutils/gnubug-25003/setup.sh
+++ experiments/extractfix/coreutils/gnubug-25003/setup.sh
@@ -13,11 +13,17 @@ git clone $project_url src
 cd src
 git checkout $commit_id
 
+sed -i '987i TRIDENT_OUTPUT("observation", "i32", initial_read - start);\n' src/split.c
+sed -i '985d' src/split.c
+sed -i '985i if(__trident_choice("L290", "bool", (int[]){size, initial_read}, (char*[]){"size","initial_read"}, 2, (int*[]){}, (char*[]){}, 0)))' src/split.c
+git add src/split.c
+git commit -m "instrument trident"
 
+./bootstrap
 
 cd $current_dir
 cp repair.conf $dir_name
 cp spec.smt2 $dir_name
 cp t1.smt2 $dir_name
 cp -rf components $dir_name
-cp exploit.txt $dir_name
+
