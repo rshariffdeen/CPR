911ba1cbf98eca6bfa5b41f181767213313fb796
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-09-21 02:18:36 +0800

    remove linking

--- main/concolic.py
+++ main/concolic.py
@@ -544,7 +544,6 @@ def run_concolic_execution(program, argument_list, second_var_list, print_output
                    "--log-ppc " \
                    "--log-trace " \
                    + "--external-calls=all " \
-                   + "--link-llvm-lib={0} ".format(definitions.FILE_TRIDENT_LIB_BCA) \
                    + "{0}".format(hit_location_flag) \
                    + "--max-forks {0} ".format(values.DEFAULT_MAX_FORK) \
                    + values.CONF_KLEE_FLAGS + " " \
@@ -598,7 +597,6 @@ def run_concrete_execution(program, argument_list, print_output=False, output_di
                     "--log-ppc " \
                     "--external-calls=all " \
                     "--max-forks {0} ".format(values.DEFAULT_MAX_FORK) \
-                    + "--link-llvm-lib={0} ".format(definitions.FILE_TRIDENT_LIB_BCA) \
                     + values.CONF_KLEE_FLAGS + " " \
                     + "{0} ".format(binary_name) \
                     + input_argument
