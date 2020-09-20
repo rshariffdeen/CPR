1f0dff6c2548ba69a8eacceb98ed3fcaec0534ae
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-09-20 18:03:20 +0800

    change to adn

--- experiments/extractfix/coreutils/bugzilla-26545/setup.sh
+++ experiments/extractfix/coreutils/bugzilla-26545/setup.sh
@@ -12,10 +12,9 @@ git clone $project_url src
 cd src
 git checkout $commit_id
 
-
 sed -i '292i TRIDENT_OUTPUT("i", "i32", i);\n' src/shred.c
 sed -i '290d' src/shred.c
-sed -i '290i for(i = 3; i < size && (__trident_choice("L290", "bool", (int[]){size, i}, (char*[]){"size","i"}, 2, (int*[]){}, (char*[]){}, 0)); i *= 2)' src/shred.c
+sed -i '290i for(i = 3; (__trident_choice("L290", "bool", (int[]){size, i}, (char*[]){"size","i"}, 2, (int*[]){}, (char*[]){}, 0)); i *= 2)' src/shred.c
 git add src/shred.c
 git commit -m "instrument trident"
 
@@ -29,4 +28,3 @@ cp spec.smt2 $dir_name
 cp t1.smt2 $dir_name
 cp -rf components $dir_name
 cp exploit.txt $dir_name
-
