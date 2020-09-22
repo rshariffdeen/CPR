60356a1ac24207a26393d06dde609ef7443afe1c
rshariffdeen <rshariffdeen@gmail.com>
2020-09-22 15:00:09 +0800

    keep array size for A-data

--- main/concolic.py
+++ main/concolic.py
@@ -61,6 +61,9 @@ def z3_get_model(formula):
             max_index = max(list(byte_list.keys()))
             if var_name in values.LIST_BIT_LENGTH:
                 array_size = values.LIST_BIT_LENGTH[var_name] - 1
+                if var_name not in ["A-data", "A-data-stat"]:
+                    array_size = values.LIST_BIT_LENGTH[var_name]
+
             else:
                 array_size = max_index + 1  # TODO: this could be wrong calculation
 
