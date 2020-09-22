28e75b94bb619d8a597fc1ca9b04432293766b93
rshariffdeen <rshariffdeen@gmail.com>
2020-09-22 12:01:53 +0800

    add proxy library

--- experiments/extractfix/libtiff/bugzilla-2611/setup.sh
+++ experiments/extractfix/libtiff/bugzilla-2611/setup.sh
@@ -4,11 +4,9 @@ dir_name=$1/extractfix/$project_name/$bug_id
 project_url=https://github.com/vadz/libtiff.git
 commit_id=9a72a69
 
-
+current_dir=$PWD
 mkdir -p $dir_name
 cd $dir_name
 git clone $project_url src
+cd src
 git checkout $commit_id
-cp exploit.tif $dir_name
-
-
---
+++ lib/proxy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+float fabs_trident(float a);
+
+float fabs_trident(float a){
+
+  if (a > 0){
+     return 10;
+  }
+  return 0;
+}
+
