1d93e5ffad38ba5de3b8b76eba99bbdd7eff8852
rshariffdeen <rshariffdeen@gmail.com>
2020-09-25 14:38:47 +0800

    insert klee_abort

--- experiments/patchweave/openjpeg/github-169/setup.sh
+++ experiments/patchweave/openjpeg/github-169/setup.sh
@@ -23,7 +23,7 @@ sed -i "s/get_file_format(infile)/$opj_input/g" $opj_file
 git add $opj_file
 git commit -m "fix input format"
 
-sed -i '470i TRIDENT_OUTPUT("obs", "i32", cp->tdx * cp->tdy); exit(255);' libopenjpeg/j2k.c
+sed -i '470i TRIDENT_OUTPUT("obs", "i32", cp->tdx * cp->tdy); klee_abort();' libopenjpeg/j2k.c
 sed -i '470i if(__trident_choice("L515", "bool", (int[]){cp->tdx,cp->tdy}, (char*[]){"x", "y"}, 2, (int*[]){}, (char*[]){}, 0)) return;' libopenjpeg/j2k.c
 sed -i '35i #ifndef TRIDENT_OUTPUT\n#define TRIDENT_OUTPUT(id, typestr, value) value\n#endif\n' libopenjpeg/j2k.c
 git add libopenjpeg/j2k.c
