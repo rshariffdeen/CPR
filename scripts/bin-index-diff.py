a371c7b48fabac1d02db26708b048e861648708d
rshariffdeen <rshariffdeen@gmail.com>
2020-09-30 13:46:25 +0800

    add script to get bin diff in file

--- scripts/bin-index-diff.py
+++ scripts/bin-index-diff.py
@@ -1,6 +1,35 @@
 import sys
+import struct
+
+file_path_a = sys.argv[1]
+file_path_b = sys.argv[2]
+
+byte_list_a = dict()
+byte_list_b = dict()
 
 
-file_a = sys.argv[1]
-file_b = sys.argv[2]
+with open(file_path_a, "rb") as file_a:
+    byte = file_a.read(1)
+    index_a = 0
+    while byte:
+        number = int(struct.unpack('>B', byte)[0])
+        byte_list_a[index_a] = number
+        index_a = index_a + 1
+        byte = file_a.read(1)
+
+with open(file_path_b, "rb") as file_b:
+    byte = file_b.read(1)
+    index_b = 0
+    while byte:
+        number = int(struct.unpack('>B', byte)[0])
+        byte_list_b[index_b] = number
+        index_b = index_b + 1
+        byte = file_b.read(1)
+
+diff_list = list()
+for index in byte_list_a:
+    if byte_list_a[index_a] != byte_list_b[index_a]:
+        diff_list.append(index_a)
+
+print(diff_list)
 
