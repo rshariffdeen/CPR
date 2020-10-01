ef8f6f2a956a496566641be948d83c72e69843a8
rshariffdeen <rshariffdeen@gmail.com>
2020-10-01 14:03:17 +0800

    remove over-approximated patches

--- main/oracle.py
+++ main/oracle.py
@@ -85,8 +85,7 @@ def check_patch_feasibility(assertion, var_relationship, patch_constraint, path_
             else:
                 result = universal_quantification
         else:
-            existential_quantification = is_unsat(And(specification, assertion))
-            result = existential_quantification
+            result = is_sat(specification)
     else:
         result = is_sat(specification)
 
