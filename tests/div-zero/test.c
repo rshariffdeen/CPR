4eae593758bf2b3246cfa6babdc59f569e49eb44
rshariffdeen <rshariffdeen@gmail.com>
2020-10-01 12:09:19 +0800

    update test case

--- tests/div-zero/test.c
+++ tests/div-zero/test.c
@@ -6,17 +6,18 @@
 
 int main(int argc, char *argv[]) {
   int x = atoi(argv[1]);
+  int z = atoi(argv[2]);
   int res, y;
   if (__trident_choice("L9", "bool", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0))  {
       return -1;
   }
 
-  if (x > 0)
+  if (z > 10)
     y = x - 1;
   else
     y = x + 2;
 
   TRIDENT_OUTPUT("y", "i32", y);
-  res = 1000 / y;
+  res = z / y;
   return 0;
 }
