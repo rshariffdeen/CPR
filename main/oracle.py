e6a5205daab046b3255fd5135b42cb33631b9b0d
rshariffdeen <rshariffdeen@gmail.com>
2020-10-01 13:00:43 +0800

    remove over-approximated patches

--- main/oracle.py
+++ main/oracle.py
@@ -74,14 +74,10 @@ def check_patch_feasibility(assertion, var_relationship, patch_constraint, path_
     if assertion:
         if is_loc_in_trace(values.CONF_LOC_BUG):
             universal_quantification = is_unsat(And(specification, Not(assertion)))
-            if universal_quantification:
-                specification = And(path_condition, Not(patch_constraint))
-                existential_quantification = is_unsat(And(specification, assertion))
-                result = universal_quantification and existential_quantification
-            else:
-                result = universal_quantification
+            result = universal_quantification
         else:
-            result = is_sat(specification)
+            existential_quantification = is_unsat(And(specification, assertion))
+            result = existential_quantification
     else:
         result = is_sat(specification)
 
