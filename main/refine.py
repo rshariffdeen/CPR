67754e80bb5b356d7755eaffee490d0b19bd36ad
rshariffdeen <rshariffdeen@gmail.com>
2020-10-19 19:19:33 +0800

    use recursion instead of loop

--- main/refine.py
+++ main/refine.py
@@ -23,19 +23,16 @@ def refine_for_under_approx(p_specification, patch_constraint, path_condition):
     path_feasibility = And(path_condition, patch_space_constraint)
     specification = And(path_feasibility, Not(p_specification))
     universal_quantification = is_unsat(specification)
-    refined_constant_space = constant_space
+    refined_constant_space = None
     if not universal_quantification:
-        while not universal_quantification:
-            emitter.debug("refining for universal quantification")
-            model = generator.generate_model(specification)
-            refined_constant_space = refine_constant_range(constant_space, model, path_condition, patch_constraint, Not(p_specification))
-            if refined_constant_space is None:
-                break
-            constant_constraint = generator.generate_constant_constraint_formula(refined_constant_space)
-            patch_space_constraint = And(patch_constraint, constant_constraint)
-            path_feasibility = And(path_condition, patch_space_constraint)
-            specification = And(path_feasibility, Not(p_specification))
-            universal_quantification = is_unsat(specification)
+        emitter.debug("refining for universal quantification")
+        model = generator.generate_model(specification)
+        refined_partition_list = refine_constant_range(model, constant_space, path_condition, patch_constraint, Not(p_specification))
+        if refined_partition_list:
+            if len(refined_partition_list) == 1:
+                refined_constant_space = refined_partition_list[0]
+            for parition in refined_partition_list:
+                print(parition)
     return refined_constant_space
 
 
@@ -54,7 +51,7 @@ def refine_for_over_approx(p_specification, patch_constraint, path_condition):
         while not existential_quantification:
             emitter.debug("refining for existential quantification")
             model = generator.generate_model(specification)
-            refined_constant_space = refine_constant_range(constant_space, model, path_condition, patch_constraint, p_specification)
+            refined_constant_space = refine_constant_range(model, constant_space, path_condition, patch_constraint, p_specification)
             if refined_constant_space is None:
                 break
             constant_constraint = generator.generate_constant_constraint_formula(refined_constant_space)
@@ -187,23 +184,16 @@ def generate_formula_for_range(patch, constant_space, path_condition, assertion)
     return formula
 
 
-def refine_constant_range(constant_space, model, path_condition, patch_constraint, p_specification):
+def refine_constant_range(model, constant_space, path_condition, patch_constraint, p_specification):
     refined_patch = None
     constant_count = len(constant_space)
+    refined_list = []
     refined_constant_space = None
     partition_list = dict()
 
     for var_name in model:
         if "const_" in var_name:
             partition_list[var_name] = int(model[var_name][0])
-        if "rvalue" in var_name:
-            bit_vector = model[var_name]
-            contradiction_value = utilities.get_signed_value(bit_vector)
-            array = Symbol(var_name, ArrayType(BV32, BV8))
-            array_value = BVConcat(Select(array, BV(3, 32)),
-                 BVConcat(Select(array, BV(2, 32)),
-                 BVConcat(Select(array, BV(1, 32)), Select(array, BV(0, 32)))))
-            assertion = Equals(array_value, BV(contradiction_value, 32))
 
     range_list = generate_new_range(constant_space, partition_list)
     for partition_range in range_list:
@@ -221,19 +211,16 @@ def refine_constant_range(constant_space, model, path_condition, patch_constrain
         constant_constraint = generator.generate_constant_constraint_formula(partitioned_constant_space)
         patch_space_constraint = And(patch_constraint, constant_constraint)
         path_feasibility = And(path_condition, patch_space_constraint)
-        is_exist_verification = And(path_feasibility, assertion)
-        is_always_verification = And(path_feasibility, p_specification)
-        if is_sat(is_exist_verification):
-            if is_sat(is_always_verification):
-                if refined_constant_space is None:
-                    refined_constant_space = partitioned_constant_space
-                    emitter.data("refined space", refined_constant_space)
-                    return refined_constant_space
-        else:
-            refined_constant_space = partitioned_constant_space
-            emitter.data("refined space", refined_constant_space)
-            return refined_constant_space
+        is_exist_verification = And(path_feasibility, p_specification)
 
-    emitter.data("refined space", refined_constant_space)
-    return refined_constant_space
+        if is_sat(is_exist_verification):
+            model = generator.generate_model(And(path_feasibility, p_specification))
+            child_list = refine_constant_range(model, partitioned_constant_space, path_condition, patch_constraint, p_specification)
+            refined_list = refined_list + child_list
+        else:
+            emitter.data("refined space", refined_constant_space)
+            refined_list.append(partitioned_constant_space)
+
+    emitter.data("refined list", refined_list)
+    return refined_list
 
