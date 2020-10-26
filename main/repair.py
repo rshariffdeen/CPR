049e22caf6eccdced1244229318e183c1e7411f8
rshariffdeen <rshariffdeen@gmail.com>
2020-10-26 20:05:04 +0800

    merge only if input space exist

--- main/repair.py
+++ main/repair.py
@@ -19,11 +19,9 @@ def reduce(patch_list: List[Dict[str, Program]], path_to_concolic_exec_result: s
     expr_log_path = str(path_to_concolic_exec_result) + "/expr.log"
     path_condition = extractor.extract_assertion(path_constraint_file_path)
     input_space = parallel.partition_input_space(path_condition, assertion)
-    if input_space:
-        input_space = merger.merge_space(input_space)
-    print(input_space)
     if not input_space:
         return patch_list
+    input_space = merger.merge_space(input_space)
     if values.CONF_PATCH_TYPE == values.OPTIONS_PATCH_TYPE[1]:
         result_list = parallel.refine_patch_space(patch_list, path_condition, assertion)
         if not result_list:
