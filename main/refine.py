d9ba6fa717cb753050609e32536ced5739fedbbd
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-10-30 12:52:19 +0800

    fix bug that discard non-parameter patches without checking

--- main/refine.py
+++ main/refine.py
@@ -126,7 +126,9 @@ def refine_for_over_fit(patch_formula, path_condition, negated_path_condition, p
 
 def refine_for_under_approx(patch_formula, path_condition, patch_space, p_specification):
     parameter_constraint = generator.generate_constraint_for_patch_space(patch_space)
-    patch_space_constraint = And(patch_formula, parameter_constraint)
+    patch_space_constraint = patch_formula
+    if parameter_constraint:
+        patch_space_constraint = And(patch_formula, parameter_constraint)
     path_feasibility = And(path_condition, patch_space_constraint)
     path_constraint = And(path_condition, patch_formula)
     if values.VALID_INPUT_SPACE:
@@ -148,7 +150,9 @@ def refine_for_under_approx(patch_formula, path_condition, patch_space, p_specif
 
 def refine_for_over_approx(patch_formula, path_condition, patch_space, p_specification):
     parameter_constraint = generator.generate_constraint_for_patch_space(patch_space)
-    patch_space_constraint = And(patch_formula, parameter_constraint)
+    patch_space_constraint = patch_formula
+    if parameter_constraint:
+        patch_space_constraint = And(patch_formula, parameter_constraint)
     path_feasibility = And(path_condition, patch_space_constraint)
     path_constraint = And(path_condition, patch_formula)
     if values.VALID_INPUT_SPACE:
