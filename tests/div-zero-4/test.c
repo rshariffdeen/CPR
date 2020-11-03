00d9c815fb354b6e4b4362d3c2bb45d2a06cd52a
rshariffdeen <rshariffdeen@gmail.com>
2020-11-03 10:53:48 +0800

    update test case

--- tests/div-zero-4/test.c
+++ tests/div-zero-4/test.c
@@ -10,9 +10,9 @@ int main(int argc, char *argv[]) {
 
   int res, z;
   if (x > 5)
-    z = y - 1;
+    y = y - 1;
   else
-    z = y + 2;
+    y = y + 2;
 
 
   if (__trident_choice("L9", "bool", (int[]){x, y}, (char*[]){"x", "y"}, 2, (int*[]){}, (char*[]){}, 0))  {
