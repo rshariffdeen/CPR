465af9f0ff4d43b673916cc24408f34b51990185
rshariffdeen <rshariffdeen@gmail.com>
2020-11-10 18:41:22 +0800

    fix bug in patch writer

--- main/writer.py
+++ main/writer.py
@@ -22,33 +22,35 @@ def write_as_pickle(data, output_file_path):
 def write_patch_set(patch_list, output_file_path):
     emitter.normal("\twriting patch list to file")
     template_count = 0
+    txt_lines = []
+    for patch in patch_list:
+        template_count = template_count + 1
+        txt_lines.append("Patch #" + str(template_count))
+        for (lid, prog) in patch.items():
+            code = lid + ": " + (program_to_code(prog))
+        txt_lines.append(code)
+        patch_formula = generator.generate_formula_from_patch(patch)
+        patch_formula_str = patch_formula.serialize()
+        patch_index = utilities.get_hash(patch_formula_str)
+        patch_score = values.LIST_PATCH_SCORE[patch_index]
+        if values.CONF_PATCH_TYPE == values.OPTIONS_PATCH_TYPE[1]:
+            patch_space = values.LIST_PATCH_SPACE[patch_index]
+            partition_count = 0
+            for partition in patch_space:
+                partition_count = partition_count + 1
+                txt_lines.append("\t\tPartition: " + str(partition_count))
+                for constant_name in partition:
+                    txt_lines.append("\t\t\tConstant: " + constant_name)
+                    constant_info = partition[constant_name]
+                    lower_bound = str(constant_info['lower-bound'])
+                    upper_bound = str(constant_info['upper-bound'])
+                    txt_lines.append("\t\t\tRange: " + lower_bound + " <= " + constant_name + " <= " + upper_bound)
+                    dimension = len(range(int(lower_bound), int(upper_bound) + 1))
+                    txt_lines.append("\t\t\tDimension: " + str(dimension))
+                    concrete_patch_count = utilities.count_concrete_patches_per_template(patch)
+        txt_lines.append("\t\tPatch Count: " + str(concrete_patch_count))
+        txt_lines.append("\t\tPath Coverage: " + str(patch_score))
+        txt_lines.append("\t\tIs Under-approximating: " + str(values.LIST_PATCH_UNDERAPPROX_CHECK[patch_index]))
+        txt_lines.append("\t\tIs Over-approximating: " + str(values.LIST_PATCH_OVERAPPROX_CHECK[patch_index]))
     with open(output_file_path, 'w') as out_file:
-        for patch in patch_list:
-            template_count = template_count + 1
-            out_file.writelines("Patch #" + str(template_count))
-            for (lid, prog) in patch.items():
-                code = lid + ": " + (program_to_code(prog))
-            out_file.writelines(code)
-            patch_formula = generator.generate_formula_from_patch(patch)
-            patch_formula_str = patch_formula.serialize()
-            patch_index = utilities.get_hash(patch_formula_str)
-            patch_score = values.LIST_PATCH_SCORE[patch_index]
-            if values.CONF_PATCH_TYPE == values.OPTIONS_PATCH_TYPE[1]:
-                patch_space = values.LIST_PATCH_SPACE[patch_index]
-                partition_count = 0
-                for partition in patch_space:
-                    partition_count = partition_count + 1
-                    out_file.writelines("\t\tPartition: " + str(partition_count))
-                    for constant_name in partition:
-                        out_file.writelines("\t\t\tConstant: " + constant_name)
-                        constant_info = partition[constant_name]
-                        lower_bound = str(constant_info['lower-bound'])
-                        upper_bound = str(constant_info['upper-bound'])
-                        out_file.writelines("\t\t\tRange: " + lower_bound + " <= " + constant_name + " <= " + upper_bound)
-                        dimension = len(range(int(lower_bound), int(upper_bound) + 1))
-                        out_file.writelines("\t\t\tDimension: " + str(dimension))
-                        concrete_patch_count = utilities.count_concrete_patches_per_template(patch)
-            out_file.writelines("\t\tPatch Count: " + str(concrete_patch_count))
-            out_file.writelines("\t\tPath Coverage: " + str(patch_score))
-            out_file.writelines("\t\tIs Under-approximating: " + str(values.LIST_PATCH_UNDERAPPROX_CHECK[patch_index]))
-            out_file.writelines("\t\tIs Over-approximating: " + str(values.LIST_PATCH_OVERAPPROX_CHECK[patch_index]))
+        out_file.writelines(txt_lines)
