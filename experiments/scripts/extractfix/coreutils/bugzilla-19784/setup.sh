546d47d237cdf16b4c0f42875d6108ecd24e9960
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-11-11 21:00:07 +0800

    update experiment

--- experiments/scripts/extractfix/coreutils/bugzilla-19784/setup.sh
+++ experiments/scripts/extractfix/coreutils/bugzilla-19784/setup.sh
@@ -20,8 +20,8 @@ git add src/make-prime-list.c
 git commit -m "instrument trident"
 
 ./bootstrap
-FORCE_UNSAFE_CONFIGURE=1 CC=$TRIDENT_CC CXX=$TRIDENT_CXX ./configure
-make -j32
+FORCE_UNSAFE_CONFIGURE=1 CC=$TRIDENT_CC CXX=$TRIDENT_CXX ./configure CFLAGS='-g -O0 -static -fPIE' CXXFLAGS="$CFLAGS"
+make CFLAGS="-fPIC -fPIE -lkleeRuntest" CXXFLAGS=$CFLAGS src/make-prime-list -j32
 
 cd $current_dir
 cp repair.conf $dir_name
