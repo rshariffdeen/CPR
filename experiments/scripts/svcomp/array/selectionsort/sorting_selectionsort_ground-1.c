b619182b19b962021283a0e11fc704b3e1d2094d
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-11-15 04:42:46 +0800

    update experiment

--- experiments/scripts/svcomp/array/selectionsort/sorting_selectionsort_ground-1.c
+++ experiments/scripts/svcomp/array/selectionsort/sorting_selectionsort_ground-1.c
@@ -7,7 +7,7 @@ void reach_error() { assert(0); }
 void __VERIFIER_assert(int cond) { if(!(cond)) { ERROR: {reach_error();abort();} } }
 extern int __VERIFIER_nondet_int();
 
-#define N 100000
+#define N 10
 
 int main( ) {
   int a[ N ];
@@ -15,10 +15,11 @@ int main( ) {
   int x;
   int y;
 
-	for(int i = 0; i < 100000; i++)
-	{
-	    a[i] = __VERIFIER_nondet_int();
-	}
+  klee_make_symbolic(&a, sizeof(a), "a");
+//	for(int i = 0; i < N; i++)
+//	{
+//	    a[i] = __VERIFIER_nondet_int();
+//	}
 
 	i = 0;
   while ( i < N ) {
