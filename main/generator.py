4518364f19e68451997ac2a59b767bf6d056ff46
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2020-11-14 17:06:43 +0800

    angelic count can be zero for paths that doesnt have angelic var

--- main/generator.py
+++ main/generator.py
@@ -841,11 +841,11 @@ def generate_assertion(assertion_temp, klee_dir):
 
 def generate_extended_patch_formula(patch_formula, path_condition):
     angelic_count = int(len(re.findall("angelic!(.+?)!0", str(path_condition.serialize()))) / 4)
-    if angelic_count == 0:
-        print("COUNT", angelic_count)
-        print("PATH", str(path_condition.serialize()))
-        utilities.error_exit("angelic count is zero in extending")
-    if angelic_count == 1:
+    # if angelic_count == 0:
+    #     print("COUNT", angelic_count)
+    #     print("PATH", str(path_condition.serialize()))
+    #     utilities.error_exit("angelic count is zero in extending")
+    if angelic_count <= 1:
         return patch_formula
     # model_path = generate_model(path_condition)
     # var_list = list(model_path.keys())
