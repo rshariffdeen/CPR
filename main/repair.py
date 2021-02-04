dca2c6dac28e0f64c1f1dfbd0391d79bf85803a3
rshariffdeen <rshariffdeen@gmail.com>
2021-02-04 16:59:48 +0800

    change emit message

--- main/repair.py
+++ main/repair.py
@@ -338,8 +338,8 @@ def run_fitreduce(program_path, patch_list):
                     emitter.warning(
                         "\t[warning] ending due to timeout of " + str(values.DEFAULT_TIME_DURATION) + " minutes")
                     break
-            emitter.note("end of concolic exploration using user-provided seeds")
-            emitter.note("\t\t|P|=" + str(utilities.count_concrete_patches(patch_list)) + ":" + str(len(patch_list)))
+            emitter.success("\t\tend of concolic exploration using user-provided seeds")
+            emitter.success("\t\t|P|=" + str(utilities.count_concrete_patches(patch_list)) + ":" + str(len(patch_list)))
             values.COUNT_PATCH_END_SEED = len(patch_list)
 
         else:
