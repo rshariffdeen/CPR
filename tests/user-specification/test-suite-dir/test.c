42c50c4514ff419396e34da376880e6711e136bb
rshariffdeen <rshariffdeen@gmail.com>
2021-02-04 19:08:11 +0800

    use fgetc insteadf of fscanf

--- tests/user-specification/test-suite-dir/test.c
+++ tests/user-specification/test-suite-dir/test.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #ifndef TRIDENT_OUTPUT
 #define TRIDENT_OUTPUT(id, typestr, value) value
 #endif
 
 int main(int argc, char *argv[]) {
   int x;
+  char ch;
   char *filepath = argv[1];
   FILE *fp = fopen(filepath, "r");
-  fscanf(fp, "%d", &x);
+  ch = fgetc(fp);
+  printf("char is: %c", ch);
+  x = atoi(&ch);
+  printf("number is: %d\n", x);
   fclose(fp);
   int y = __trident_choice("L9", "i32", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0);
   TRIDENT_OUTPUT("obs", "i32", y - (x*x));
