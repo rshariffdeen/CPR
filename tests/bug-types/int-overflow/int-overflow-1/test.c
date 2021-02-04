d208e794a4e2a8e5c32e31bc4c3d8bae06406907
rshariffdeen <rshariffdeen@gmail.com>
2021-02-04 20:37:16 +0800

    update test case

--- tests/bug-types/int-overflow/int-overflow-1/test.c
+++ tests/bug-types/int-overflow/int-overflow-1/test.c
@@ -8,7 +8,7 @@ int main(int argc, char *argv[]) {
   int x = atoi(argv[1]);
   int a = 214748364 ;
   int b;
-  if (__trident_choice("L9", "bool", (int[]){x, a}, (char*[]){"x", "a"}, 2, (int*[]){}, (char*[]){}, 0))  {
+  if (__trident_choice("L9", "bool", (int[]){x, a, INT_MAX}, (char*[]){"x", "a", "y"}, 3, (int*[]){}, (char*[]){}, 0))  {
       return -1;
   }
   TRIDENT_OUTPUT("obs", "i32", (INT_MAX/a) - x);
