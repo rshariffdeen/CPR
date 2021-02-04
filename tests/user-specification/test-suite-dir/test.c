5bda7ee35aa17e3a0af54c494c17528c17bbd4fb
rshariffdeen <rshariffdeen@gmail.com>
2021-02-04 17:16:16 +0800

    add new test case for seed dir

---
+++ tests/user-specification/test-suite-dir/test.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int main(int argc, char *argv[]) {
+  int x;
+  char *filepath = argv[1];
+  FILE *fp = fopen(filepath, "r");
+  fscanf(fp, "%d", &x);
+  fclose(fp);
+  int y = __trident_choice("L9", "i32", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0);
+  TRIDENT_OUTPUT("obs", "i32", y - (x*x));
+  return 0;
+}
