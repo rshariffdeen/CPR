3d0a845cc0d565cf938d17a3a5a9d0705e03736e
rshariffdeen <rshariffdeen@gmail.com>
2021-02-17 20:05:56 +0800

    load relative path since project path will be appended later

--- main/configuration.py
+++ main/configuration.py
@@ -336,7 +336,8 @@ def collect_test_list():
         file_list = [f for f in os.listdir(expected_output_dir) if os.path.isfile(os.path.join(expected_output_dir, f))]
         for expected_output_file in file_list:
             expected_file_abs_path = expected_output_dir + "/" + expected_output_file
-            values.LIST_TEST_OUTPUT.append(expected_file_abs_path)
+            expected_file_rel_path = str(expected_file_abs_path).replace(values.CONF_PATH_PROJECT + "/", "")
+            values.LIST_TEST_OUTPUT.append(expected_file_rel_path)
     else:
         error_exit("No expected output is given (at least one is required)")
 
