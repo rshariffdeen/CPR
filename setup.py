a47f6a82f3cec4d9cecda1f0570043b995a99484
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-03-12 17:42:40 +0800

    update setup script to remove unpackaged source files

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
+# coding: utf-8
 import os
+import fnmatch
+import sysconfig
+
 from setuptools import setup, find_packages
+from setuptools.command.build_py import build_py as _build_py
 from Cython.Build import cythonize
-from distutils.core import setup
 
 
 EXCLUDE_FILES = [
@@ -27,6 +31,21 @@ def get_ext_paths(root_dir, exclude_files):
     return paths
 
 
+# noinspection PyPep8Naming
+class build_py(_build_py):
+
+    def find_package_modules(self, package, package_dir):
+        ext_suffix = sysconfig.get_config_var('EXT_SUFFIX')
+        modules = super().find_package_modules(package, package_dir)
+        filtered_modules = []
+        for (pkg, mod, filepath) in modules:
+            if os.path.exists(filepath.replace('.py', ext_suffix)):
+                continue
+            filtered_modules.append((pkg, mod, filepath, ))
+        return filtered_modules
+
+
+
 setup(
     name='cpr',
     version='0.1.0',
@@ -34,5 +53,8 @@ setup(
     ext_modules=cythonize(
         get_ext_paths('app', EXCLUDE_FILES),
         compiler_directives={'language_level': 3}
-    )
+    ),
+    cmdclass={
+        'build_py': build_py
+    }
 )
