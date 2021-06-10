0ec6805585b54731fd67667872d2458e70371957
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-06-10 17:42:17 +0800

    add test case for multi-programs

---
+++ tests/tool-features/multiple-programs/test.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int main(int argc, char *argv[]) {
+  int x = atoi(argv[1]);
+  int y = atoi(argv[2]);
+  int b;
+  int res;
+  if (__trident_choice("L12", "bool", (int[]){x}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0))  {
+      return -1;
+  }
+
+  if (__trident_choice("L16", "bool", (int[]){y}, (char*[]){"y"}, 1, (int*[]){}, (char*[]){}, 0))  {
+      return -1;
+  }
+
+  if (x > 0)
+    b = y * x;
+  else
+    b = 2;
+  TRIDENT_OUTPUT("obs", "i32", b);
+  res = 200/b;
+  return 0;
+}
