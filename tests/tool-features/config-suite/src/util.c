4af718ff1350481499b7a348625ad9c7f1c4454f
Ridwan Shariffdeen <rshariffdeen@gmail.com>
2021-06-10 19:50:04 +0800

    add test case: testing configuration matrixes

---
+++ tests/tool-features/config-suite/src/util.c
@@ -0,0 +1,35 @@
+/* util.c */
+#include "util.h"
+#ifndef TRIDENT_OUTPUT
+#define TRIDENT_OUTPUT(id, typestr, value) value
+#endif
+
+int rand_seed=10;
+
+/* from K&R
+ - produces a random number between 0 and 32767.*/
+int rand()
+{
+    rand_seed = rand_seed * 1103515245 +12345;
+    return (unsigned int)(rand_seed / 65536) % 32768;
+}
+
+void bubble_sort(int m,int a[])
+{
+    if (__trident_choice("L12", "bool", (int[]){m}, (char*[]){"x"}, 1, (int*[]){}, (char*[]){}, 0))  {
+      return;
+    }
+    TRIDENT_OUTPUT("obs", "i32", m);
+
+    int x,y,t;
+     for (x=0; x < m-1; x++)
+        for (y=0; y < m-x-1; y++)
+            if (a[y] > a[y+1])
+            {
+                t=a[y];
+                a[y]=a[y+1];
+                a[y+1]=t;
+            }
+
+  return;
+}
---
+++ tests/tool-features/config-suite/src/util.h
@@ -0,0 +1,3 @@
+/* util.h */
+extern int rand();
+extern void bubble_sort(int, int []);
